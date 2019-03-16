2ff9beb3f2448263d393eb18b53a79473166117b
Alexander Hyriavets <alexander.hyriavets@adraba.io>
2019-03-16 22:41:57 +0200

    add search component

---
+++ src/components/ATable/ASearch.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="search-items">
+    Search: <input v-model="searchText" type="search" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SearchRecords',
+  props: {
+    value: {
+      type: String,
+      default: ''
+    }
+  },
+  computed: {
+    searchText: {
+      get() {
+        return this.value
+      },
+      set(newValue) {
+        this.$emit('input', newValue)
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.search-items input {
+  padding: 5px;
+  outline: none;
+  border: 1px solid #ddd;
+}
+</style>
